23c30f028cabe3ec320e1aca3f6959bd2df81136
Dave Tashner <dtashner@Daves-MBP.home>
2015-01-17 10:22:10 -0500

    adding cookbook to install kitchen-docs

---
+++ kitchen-docs/CHANGELOG.md
@@ -0,0 +1,13 @@
+kitchen-docs CHANGELOG
+======================
+
+This file is used to list changes made in each version of the kitchen-docs cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of kitchen-docs
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ kitchen-docs/recipes/default.rb
@@ -0,0 +1,72 @@
+#
+# Cookbook Name:: kitchen-docs
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#chef_pkg_url = "#{node[:kitchen][:chef][:url]}"
+#chef_pkg = "#{node[:kitchen][:chef][:package]}"
+
+js_runtime = "#{node[:kitchen][:javascript][:runtime][:url]}"
+js_runtime_pkg = "#{node[:kitchen][:javascript][:runtime][:package]}"
+repo = "#{node[:kitchen][:docs][:repo]}"
+
+directory "/root/.ssh" do
+	action :create
+end
+
+cookbook_file "/root/.ssh/known_hosts" do
+	action :create
+	source "known_hosts"
+end
+
+#remote_file chef_pkg do
+#	action :create
+#	source chef_pkg_url
+#end
+
+#rpm_package chef_pkg do
+#end
+
+node[:kitchen][:packages].each do |pkg|
+	package pkg do
+	action :install
+end
+end
+
+node[:gem_hash].each do |k,v|
+	gem_package k do
+	action :install
+end
+end
+
+
+execute "download code" do
+	cwd "/tmp/" 
+	command "git clone https://github.com/test-kitchen/kitchen-docs.git"
+	not_if { File.exists?("/tmp/kitchen-docs/Gemfile")}
+end
+
+#git "/tmp/kitchen-docs" do
+#	repository repo
+#	reference "master"
+#	action :checkout
+#end
+
+cookbook_file "/tmp/kitchen-docs/Gemfile" do
+	source "Gemfile"
+	action :create
+end
+
+execute "Execute Bundler Install" do
+	cwd "/tmp/kitchen-docs"
+	command "bundle install"
+end
+
+execute "Run Middleman Server" do
+	cwd "/tmp/kitchen-docs"
+	command "bundle exec middleman server -p 11899"
+end
