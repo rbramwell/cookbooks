3308c31bd22ad545b7480d82c30041086c82d8f9
Dave Tashner <dtashner@singlestoneconsulting.com>
2015-01-17 19:04:47 -0500

    gem_package block not working, reverting to bash

--- kitchen-docs/recipes/default.rb
+++ kitchen-docs/recipes/default.rb
@@ -55,15 +55,15 @@ node[:kitchen][:packages].each do |pkg|
 end
 end
 
-gem_package "nokogiri" do
-	gem_binary("/opt/chef/embedded/bin/gem")
-	options("--use-system-libraries=true --with-xml2-include=/usr/include/libxml2/")
-end
+#gem_package "nokogiri" do
+#	gem_binary("/opt/chef/embedded/bin/gem")
+#	options("--use-system-libraries=true --with-xml2-include=/usr/include/libxml2/")
+#end
 
 # Install Nokogiri with options
-#execute "Install Nokogiri" do
-#	command "opt/chef/embedded/bin/gem install nokogiri -- --use-system-libraries=true --with-xml2-include=/usr/include/libxml2/"
-#end
+execute "Install Nokogiri" do
+	command "opt/chef/embedded/bin/gem install nokogiri -- --use-system-libraries=true --with-xml2-include=/usr/include/libxml2/"
+end
 
 node[:gem_hash].each do |k,v|
 	gem_package k do
