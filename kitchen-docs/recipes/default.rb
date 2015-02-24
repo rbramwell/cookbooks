9c530041388024b5f356ced8e0d954e700b397da
Dave Tashner <dtashner@singlestoneconsulting.com>
2015-02-24 16:02:49 -0500

    adding working integration tests, commenting out final run of Middleman server as it causes Chef to hang

--- kitchen-docs/recipes/default.rb
+++ kitchen-docs/recipes/default.rb
@@ -99,12 +99,12 @@ end
 
 execute "Execute Bundler Install" do
 	cwd "/tmp/kitchen-docs"
-	command "/usr/local/bin/bundle install"
+	command "/opt/chef/embedded/bin/bundle install"
 end
 
 # Need to figure out a different server to run this site on; Middleman is a development server
-execute "Run Middleman Server" do
-	cwd "/tmp/kitchen-docs"
-	command "/usr/local/bin/bundle exec middleman server -p 11899"
-	user "root"
-end
+#execute "Run Middleman Server" do
+#	cwd "/tmp/kitchen-docs"
+#	command "/opt/chef/embedded/bin/bundle exec middleman server -p 11899"
+#	user "root"
+#end
